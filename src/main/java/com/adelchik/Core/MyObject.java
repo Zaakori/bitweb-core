e3b0af39cf39718f157c77217be4d4bf5e3b7a00
Adel Tiisler <tiisleradel@gmail.com>
2022-10-10 14:18:50 +0300

    change MyResponse class to MyObject class with fields 'word' and 'count'

---
+++ src/main/java/com/adelchik/Core/MyObject.java
@@ -0,0 +1,20 @@
+package com.adelchik.Core;
+
+public class MyObject {
+
+    private String word;
+    private int count;
+
+    public MyObject(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
--- src/main/java/com/adelchik/Core/controller/APIController.java
+++ src/main/java/com/adelchik/Core/controller/APIController.java
@@ -1,7 +1,6 @@
 package com.adelchik.Core.controller;
 
-import com.adelchik.Core.MyResponse;
-import com.adelchik.Core.db.entities.TextEntity;
+import com.adelchik.Core.MyObject;
 import com.adelchik.Core.services.FileExtractingService;
 import com.adelchik.Core.services.FileInsertingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/")
@@ -32,12 +28,12 @@ public class APIController {
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/extract")
-    public ResponseEntity<MyResponse> extractProcessedText(@RequestParam("message") String message){
-
-        MyResponse response = new MyResponse(extractingService.getStatus(message), extractingService.returnProcessedFile(message));
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
-
-    }
+//    @GetMapping(value = "/extract")
+//    public ResponseEntity<MyObject> extractProcessedText(@RequestParam("message") String message){
+//
+//        MyObject response = new MyObject(extractingService.getStatus(message), extractingService.returnProcessedFile(message));
+//
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//
+//    }
 }
